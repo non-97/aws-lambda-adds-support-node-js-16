39ef9ec593d2428bfa04108b320c87a0d9833dd9
non-97 <nonmonaka07@gmail.com>
2022-05-13 18:19:48 +0900

    initial commit

--- lib/lambda-stack.ts
+++ lib/lambda-stack.ts
@@ -1,16 +1,44 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {
+  Duration,
+  Stack,
+  StackProps,
+  aws_iam as iam,
+  aws_logs as logs,
+  aws_lambda as lambda,
+  aws_lambda_nodejs as nodejs,
+} from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as path from "path";
 
 export class LambdaStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'LambdaQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new nodejs.NodejsFunction(this, "Node.js 16 Lambda Function", {
+      entry: path.join(
+        __dirname,
+        "../src/lambda/handlers/node-js-16-handler.ts"
+      ),
+      runtime: lambda.Runtime.NODEJS_16_X,
+      bundling: {
+        minify: true,
+        sourceMap: true,
+        target: "node16.14",
+        tsconfig: path.join(__dirname, "../src/lambda/tsconfig.json"),
+        format: nodejs.OutputFormat.ESM,
+      },
+      architecture: lambda.Architecture.ARM_64,
+      role: new iam.Role(this, "IAM Role", {
+        assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+        managedPolicies: [
+          iam.ManagedPolicy.fromAwsManagedPolicyName(
+            "service-role/AWSLambdaBasicExecutionRole"
+          ),
+        ],
+      }),
+      logRetention: logs.RetentionDays.TWO_WEEKS,
+      tracing: lambda.Tracing.ACTIVE,
+      timeout: Duration.seconds(10),
+    });
   }
 }
